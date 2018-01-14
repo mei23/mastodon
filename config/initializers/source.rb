6a1fbbb897c7b6a3b61f3fe1a62a286de60475ef
mei23 <m@m544.net>
2018-01-15 06:11:17 +0900

    Add source.rb

---
+++ config/initializers/source.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+module Mastodon
+  module Version
+    module_function
+    def source_base_url
+      'https://github.com/mei23/mastodon/tree/mei1'
+    end
+  end
+end
+
